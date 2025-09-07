743eff47358894f05b0a159618e5d2180c27f5d3
dannylin314 <69590054+dannylin314@users.noreply.github.com>
2025-09-07 21:56:02 +0100

    Initial Commit: Spring Boot project setup

---
+++ src/test/java/com/danny/stocktracker/StockTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.stocktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
